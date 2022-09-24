a312c92402b4560f3724aff9c8d81ea2ac27b7ec
Max Zinni Mendes <maxzinni@gmail.com>
2022-09-24 19:17:04 -0300

    arquivos arrumados

--- LinhaAerea.py
+++ LinhaAerea.py
@@ -1,105 +1,77 @@
-# Python program for Kruskal's algorithm to find
-# Minimum Spanning Tree of a given connected,
-# undirected and weighted graph
- 
 from collections import defaultdict
- 
-# Class to represent a graph
- 
- 
-class Graph:
+
+class Grafo:
  
     def __init__(self, vertices):
-        self.V = vertices  # No. of vertices
-        self.graph = []  # default dictionary
-        # to store graph
+        self.V = vertices
+        self.grafo = []
  
-    # function to add an edge to graph
+    #Adiciona vértices no grafo
     def addEdge(self, u, v, w):
-        self.graph.append([u, v, w])
+        self.grafo.append([u, v, w])
  
-    # A utility function to find set of an element i
-    # (uses path compression technique)
+    #Localiza elemento i
     def find(self, parent, i):
         if parent[i] == i:
             return i
         return self.find(parent, parent[i])
  
-    # A function that does union of two sets of x and y
-    # (uses union by rank)
+    #Junta os elementos x e y
     def union(self, parent, rank, x, y):
         xroot = self.find(parent, x)
         yroot = self.find(parent, y)
  
-        # Attach smaller rank tree under root of
-        # high rank tree (Union by Rank)
         if rank[xroot] < rank[yroot]:
             parent[xroot] = yroot
         elif rank[xroot] > rank[yroot]:
             parent[yroot] = xroot
- 
-        # If ranks are same, then make one as root
-        # and increment its rank by one
         else:
             parent[yroot] = xroot
             rank[xroot] += 1
  
-    # The main function to construct MST using Kruskal's
-        # algorithm
+    #Funçaõ principal
     def KruskalMST(self):
  
-        result = []  # This will store the resultant MST
-         
-        # An index variable, used for sorted edges
+        resultado = []
         i = 0
-         
-        # An index variable, used for result[]
         e = 0
  
-        # Step 1:  Sort all the edges in
-        # non-decreasing order of their
-        # weight.  If we are not allowed to change the
-        # given graph, we can create a copy of graph
-        self.graph = sorted(self.graph,
+        # Arruma os pesos em ordem decrescente
+        self.grafo = sorted(self.grafo,
                             key=lambda item: item[2])
  
         parent = []
         rank = []
  
-        # Create V subsets with single elements
+        #Cria subsets
         for node in range(self.V):
             parent.append(node)
             rank.append(0)
  
-        # Number of edges to be taken is equal to V-1
+        # Numero de arestas são igual a vértices-1
         while e < self.V - 1:
- 
-            # Step 2: Pick the smallest edge and increment
-            # the index for next iteration
-            u, v, w = self.graph[i]
+
+            # Pega o menor valor de aresta e incrementa
+            # seu indice para iteração
+            u, v, w = self.grafo[i]
             i = i + 1
             x = self.find(parent, u)
             y = self.find(parent, v)
  
-            # If including this edge does't
-            #  cause cycle, include it in result
-            #  and increment the indexof result
-            # for next edge
+            # Se incluir o vértice não criar um ciclo
+            # incementa o indice para o proximo vertice
             if x != y:
                 e = e + 1
-                result.append([u, v, w])
+                resultado.append([u, v, w])
                 self.union(parent, rank, x, y)
-            # Else discard the edge
+        custoMinimo = 0
+        print ("Distâncias entre as cidades")
+        for u, v, peso in resultado:
+            custoMinimo += peso
+            print("%d -- %d == %d" % (u, v, peso))
+        print("Custo mínimo de cobertura" , custoMinimo)
  
-        minimumCost = 0
-        print ("Edges in the constructed MST")
-        for u, v, weight in result:
-            minimumCost += weight
-            print("%d -- %d == %d" % (u, v, weight))
-        print("Minimum Spanning Tree" , minimumCost)
- 
-# Driver code
-g = Graph(7)
+g = Grafo(7)
 g.addEdge(0, 1, 2704)
 g.addEdge(0, 2, 1846)
 g.addEdge(0, 4, 337)
@@ -114,7 +86,4 @@ g.addEdge(4, 5, 1235)
 g.addEdge(4, 6, 2342)
 g.addEdge(5, 6, 1121)
  
-# Function call
 g.KruskalMST()
- 
-# This code is contributed by Neelam Yadav
